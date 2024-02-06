36475ec0d303c6c7b79f4fb84194bd209cecd719
Ankit Kumar Jha <ankitjha2701@gmail.com>
2024-02-06 20:39:21 +0530

    Create findLengthOfLCIS.java
    Given an unsorted array of integers nums, return the length of the longest continuous increasing subsequence (i.e. subarray). The subsequence must be strictly increasing.
    
    A continuous increasing subsequence is defined by two indices l and r (l < r) such that it is [nums[l], nums[l + 1], ..., nums[r - 1], nums[r]] and for each l <= i < r, nums[i] < nums[i + 1].

---
+++ findLengthOfLCIS.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        int count=1;
+        int ans=-1;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]<nums[i+1]){
+                count++;
+            }
+            else{
+                ans=Math.max(ans,count);
+                count=1;
+            }
+        }
+        return Math.max(ans,count);
+    }
+}
