c207f7280d4896486cda70ded744d31d37a467b8
Ankit Kumar Jha <ankitjha2701@gmail.com>
2024-03-28 13:17:51 +0530

    Create relativeSortArray.java
    Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
    
    Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.

---
+++ relativeSortArray.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        TreeMap<Integer,Integer> map=new TreeMap<>();
+        for(int i=0;i<arr1.length;i++){
+            if(map.containsKey(arr1[i])){
+                map.put(arr1[i],map.get(arr1[i])+1);
+            }
+            else{
+                map.put(arr1[i],1);
+            }
+        }
+        int[] arr=new int[arr1.length];
+        int k=0;
+        for(int i=0;i<arr2.length;i++){
+            for(int j=0;j<map.get(arr2[i]);j++){
+                arr[k]=arr2[i];
+                k++;
+            }
+            map.remove(arr2[i]);
+        }
+        Set<Integer> keySet=map.keySet();
+        for(int key: map.keySet()){
+            for(int i=0;i<map.get(key);i++){
+                arr[k]=key;
+                k++;
+            }
+        }
+        return arr;
+    }
+}
