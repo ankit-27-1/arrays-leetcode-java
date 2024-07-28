89538c57625521b3586b496bef9cff03b7df1005
Ankit Kumar Jha <ankitjha2701@gmail.com>
2024-07-28 18:55:01 +0530

    Create FindIfDigitGameCanBeWon.java
    You are given an array of positive integers nums.
    
    Alice and Bob are playing a game. In the game, Alice can choose either all single-digit numbers or all double-digit numbers from nums, and the rest of the numbers are given to Bob. Alice wins if the sum of her numbers is strictly greater than the sum of Bob's numbers.
    
    Return true if Alice can win this game, otherwise, return false.

---
+++ FindIfDigitGameCanBeWon.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int a=0;
+        int b=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>9){
+                b+=nums[i];
+            }
+            else{
+                a+=nums[i];
+            }
+        }
+        return a!=b;
+    }
+}
